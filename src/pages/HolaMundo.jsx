2f05bd75773e66975869193f4431c2db7b9a518e
juacax <mateos.joaquin@gmail.com>
2023-07-11 14:17:17 -0300

    first commit

---
+++ src/pages/HolaMundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      {/* Aquí puedes agregar el contenido adicional de tu página */}
+    </div>
+  );
+}
+
+export default HolaMundo;
