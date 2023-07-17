76e1faa366c7b4d3de114c68bc82298577fd5f8e
jmateos-incluit <joaquin.mateos@incluit.com>
2023-07-17 11:44:32 -0300

    scanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,47 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Quagga from 'quagga';
+import { BrowserMultiFormatReader,BarcodeFormat } from "@zxing/library";
 
-const App = (props) => {
+const App = () => {
 
-  const firstUpdate = useRef(true);
-  const [isStart, setIsStart] = useState(false);
-  const [barcode, setBarcode] = useState('');
+  const videoRef = useRef(null);
+  const [barcode, setBarcode] = useState("");
 
   useEffect(() => {
+    let contador=0;
+    const hints = new Map();
+    const formats = [BarcodeFormat.PDF_417];
+    hints.set(formats);
+
+    const codeReader = new BrowserMultiFormatReader(hints);
+    const devices = codeReader.listVideoInputDevices()
+    codeReader.format
+    codeReader.decodeFromVideoDevice(devices[0],videoRef.current,(result,error)=>{
+
+      if(result){
+        contador++;
+        console.log("inteto "+contador);
+        if(result?.format == 10){
+          console.log(result)
+          console.log(JSON.stringify(result.resultMetadata))
+          setBarcode(result.text)
+        }else{
+          setBarcode("Código no válido, formato número: " + result.format)
+        }
+      }
+    })
+
     return () => {
-      if (isStart) stopScanner();
+      codeReader.reset();
     };
   }, []);
 
-  useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      return;
-    }
-
-    if (isStart) startScanner();
-    else stopScanner();
-  }, [isStart]);
-
-  const _onDetected = res => {
-    // stopScanner();
-    setBarcode(res.codeResult.code);
-  };
-
-  const startScanner = () => {
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          target: document.querySelector('#scanner-container'),
-          constraints: {
-            facingMode: 'environment' // or user
-          }
-        },
-        numOfWorkers: navigator.hardwareConcurrency,
-        locate: true,
-        frequency: 1,
-        debug: {
-          drawBoundingBox: true,
-          showFrequency: true,
-          drawScanline: true,
-          showPattern: true
-        },
-        multiple: false,
-        locator: {
-          halfSample: false,
-          patchSize: 'large', // x-small, small, medium, large, x-large
-          debug: {
-            showCanvas: false,
-            showPatches: false,
-            showFoundPatches: false,
-            showSkeleton: false,
-            showLabels: false,
-            showPatchLabels: false,
-            showRemainingPatchLabels: false,
-            boxFromPatches: {
-              showTransformed: false,
-              showTransformedBox: false,
-              showBB: false
-            }
-          }
-        },
-        decoder: {
-          readers: props.readers
-        }
-      },
-      err => {
-        if (err) {
-          return console.log(err);
-        }
-        Quagga.start();
-      }
-    );
-    Quagga.onDetected(_onDetected);
-    Quagga.onProcessed(result => {
-      let drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-
-      if (result) {
-        if (result.boxes) {
-          drawingCtx.clearRect(
-            0,
-            0,
-            parseInt(drawingCanvas.getAttribute('width')),
-            parseInt(drawingCanvas.getAttribute('height'))
-          );
-          result.boxes.filter(box => box !== result.box).forEach(box => {
-            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-              color: 'green',
-              lineWidth: 2
-            });
-          });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-        }
-      }
-    });
-  };
-
-  const stopScanner = () => {
-    Quagga.offProcessed();
-    Quagga.offDetected();
-    Quagga.stop();
-  };
-
-  return <div>
-    <h3>Barcode scanner in React - <a href="https://www.cluemediator.com/" target="_blank">Clue Mediator</a></h3>
-    <button onClick={() => setIsStart(prevStart => !prevStart)} style={{ marginBottom: 20 }}>{isStart ? 'Stop' : 'Start'}</button>
-    {isStart && <React.Fragment>
-      <div id="scanner-container" />
-      <span>Barcode: {barcode}</span>
-    </React.Fragment>}
-  </div>
+  return (
+    <div>
+      <h1>Lector de codigos</h1>
+      <p>Lectura: {barcode}</p>
+      <video ref={videoRef} style={{ width: '100%', height: 'auto' }} />
+    </div>
+  );
 }
 
 export default App;
